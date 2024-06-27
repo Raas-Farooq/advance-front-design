472b9353c07f4fdacac2d9648dadc69c17bd22a2
Raas-Farooq <raasfarooq@gmail.com>
2024-06-27 06:26:01 +0500

    add background and styles

--- src/body.js
+++ src/body.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Body(){
 
     return (
-        <div style={{position:"absolute", top:"0", zIndex:"1"}}>
-             <img src="https://images.pexels.com/photos/2166711/pexels-photo-2166711.jpeg?cs=srgb&dl=pexels-quang-nguyen-vinh-222549-2166711.jpg&fm=jpg" width="100%" height="800px" />
+        <div style={{position:"fixed", top:"0", left:0, right:0, bottom:0, zIndex:-1}}>
+             <img src="https://images.pexels.com/photos/2166711/pexels-photo-2166711.jpeg?cs=srgb&dl=pexels-quang-nguyen-vinh-222549-2166711.jpg&fm=jpg" width="100%" height="100%" style={{objectFit:"cover"}} />
         </div>
     )
 }
